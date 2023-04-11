ff301f2a0fcc34c09de25a3565a8f66d5f37db97
Marcel Widmer <marcel.widmer@helsana.ch>
2023-04-11 09:10:16 +0200

    chore: upgrade springboot 2.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,10 @@ import kotlin.properties.ReadOnlyProperty
 description = "Spring Boot Starter"
 
 plugins {
-    id("org.springframework.boot") version "2.7.4" apply false
-    id("io.spring.dependency-management") version "1.0.14.RELEASE" apply false
-    kotlin("jvm") version "1.6.0" apply false
-    kotlin("plugin.spring") version "1.6.0" apply false
+    id("org.springframework.boot") version "2.7.10" apply false
+    id("io.spring.dependency-management") version "1.0.15.RELEASE" apply false
+    kotlin("jvm") version "1.8.20" apply false
+    kotlin("plugin.spring") version "1.8.20" apply false
     base
     id("idea")
     id("org.jmailen.kotlinter") version "3.3.0"
